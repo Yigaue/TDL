997b52b469f23d7f7c847535ffe90a82ee27421d
Lekia <lekiagospel@gmail.com>
2020-10-01 15:59:37 +0100

    first completion  of todos

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Codos from './todos';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [
+      {id: 1, content: 'Read a book'},
+      {id: 2, content: 'attend classes'}
+    ]
+  }
+  deleteTodo = (id) => {
+    const todos  = this.state.todos.filter(todo => {
+      return todo.id !== id;
+    });
+    this.setState({
+      // todos: todos would have been the case but since the key and the 
+      // value have the same name we can write it just like below (ES6 short hand)
+      todos
+    })
+  }
+
+  render() {
+    return (
+      <div className="todos-app container">
+      <h1 className="center blue-text">Todo's</h1>
+      <Codos todos={this.state.todos} deleteTodo={this.deleteTodo}></Codos>
+      </div>
+    );
+  }
 }
 
 export default App;
